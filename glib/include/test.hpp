5a2e82fa07052af71d9df748ca423677cadefe89
cesarus777 <ulius.cesarus@gmail.com>
2020-03-23 17:23:55 +0300

    add test macro

--- glib/include/test.hpp
+++ glib/include/test.hpp
@@ -1,4 +1,27 @@
 #pragma once
 
-//psk
+#include <iostream>
+
+#include <macro/error_handler.hpp>
+
+#define START_TEST_CASE(test_name) \
+  std::cout << "Testing : " << test_name << std::endl;
+
+#define END_TEST_CASE \
+  std::cout << "Tested successful" << std::endl;
+
+#define CHECK(expr) \
+  expr ? void : bad_check(expr);
+
+#define START_TEST_MODULE(module_name) \
+  std::cout << "Testing module : " << module_name << std::endl;
+
+#define END_TEST_MODULE \
+  std::cout << "Tested successful" << std::endl;
+
+#define START_TEST_SUBMODULE(submodule_name) \
+  std::cout << "\tTesting submodule : " << submodule_name << std::endl;
+
+#define END_TEST_SUBMODULE \
+  std::cout << "\tTested successful" << std::endl;
 
