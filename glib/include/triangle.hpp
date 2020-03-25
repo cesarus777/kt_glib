4b1c05c6ecae1676814c56663989edabdf6a9c33
cesarus777 <ulius.cesarus@gmail.com>
2020-03-25 08:45:18 +0300

    fix ctr

--- glib/include/triangle.hpp
+++ glib/include/triangle.hpp
@@ -9,7 +9,7 @@ namespace mygeo
   public:
     Triangle(Point p1, Point p2, Point p3) : Polygon({ p1, p2, p3 }, TRIANGLE) {}
 
-    Triangle(std::vector<double> v) : Polygon(v, TRIANGLE)
+    Triangle(std::vector<Point> v) : Polygon(v, TRIANGLE)
     {
       if(v.size() != 3)
         throw std::invalid_argument("Invalid Triangle : wrong number of points");
