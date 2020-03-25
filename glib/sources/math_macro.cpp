5a6890eff7abc04022490f4587bb80d9454cbe2b
cesarus777 <ulius.cesarus@gmail.com>
2020-03-25 05:54:04 +0300

    make corner cases work correctly

--- glib/sources/math_macro.cpp
+++ glib/sources/math_macro.cpp
@@ -6,6 +6,10 @@ namespace my_math
 {
   double tan_sum(double tan1, double tan2)
   {
+    if(tan1 == INFINITY)
+      return 1 / tan2;
+    if(tan2 == INFINITY)
+      return 1 / tan1;
     return ((tan1 + tan2) / (1.0 - tan1 * tan2));
   }
 }
