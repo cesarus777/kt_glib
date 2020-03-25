c0c24e4179cc5226dfd3e2354b2be1d00eac6e7f
cesarus777 <ulius.cesarus@gmail.com>
2020-03-25 08:39:48 +0300

    Initial commit

---
+++ glib/include/triangle.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <base.hpp>
+
+namespace mygeo
+{
+  class Triangle : public Polygon
+  {
+  public:
+    Triangle(Point p1, Point p2, Point p3) : Polygon({ p1, p2, p3 }, TRIANGLE) {}
+
+    Triangle(std::vector<double> v) : Polygon(v, TRIANGLE)
+    {
+      if(v.size() != 3)
+        throw std::invalid_argument("Invalid Triangle : wrong number of points");
+    }
+  };
+}
