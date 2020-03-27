737fc3173fcf46d19a9423d351ec1094ad1af2c7
cesarus777 <ulius.cesarus@gmail.com>
2020-03-27 23:29:24 +0300

    add unit test for includes

---
+++ glib/include/t/unit_test.cpp
@@ -0,0 +1,40 @@
+#include <array>
+#include <iostream>
+
+#include <macro/error_handler.hpp>
+#include <macro/math_macro.hpp>
+#include <macro/test.hpp>
+
+void test_base_hpp();
+void test_circle_hpp();
+void test_ellipse_hpp();
+void test_polygon_hpp();
+void test_rectangle_hpp();
+void test_square_hpp();
+void test_triangle_hpp();
+
+void test_includes()
+{
+  test_base_hpp();
+  test_circle_hpp();
+  test_ellipse_hpp();
+  test_polygon_hpp();
+  test_rectangle_hpp();
+  test_square_hpp();
+  test_triangle_hpp();
+}
+
+void test_base_hpp() {}
+
+void test_circle_hpp() {}
+
+void test_ellipse_hpp() {}
+
+void test_polygon_hpp() {}
+
+void test_rectangle_hpp() {}
+
+void test_square_hpp() {}
+
+void test_triangle_hpp() {}
+
